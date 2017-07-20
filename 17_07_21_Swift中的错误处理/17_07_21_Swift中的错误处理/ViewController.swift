e13af7427254e46f5ca6bf766ae4f369f44c2238
YihengYao <fn1117@qq.com>
2017-07-21 08:12:31 +1200

    17_07_21_错误处理
    处理错误的三种try

--- 17_07_21_Swift中的错误处理/17_07_21_Swift中的错误处理/ViewController.swift
+++ 17_07_21_Swift中的错误处理/17_07_21_Swift中的错误处理/ViewController.swift
@@ -12,7 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let jsonString = "{\"name\": \"Zhangsan\"}"
+        //将字符串转换成data
+        let data = jsonString.data(using: .utf8)
+
+        //反序列化 throw抛出异常
+        //方法一: 推荐使用try? 如果解析成功, 则有值, 否则为nil
+        let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+        print(json)
+
+        //方法二: 不推荐使用try!如果解析成功, 则有值, 否则fatal error 程序崩溃
+        let json2 = try! JSONSerialization.jsonObject(with: data!, options: [])
+        print(json)
+
+        //方法三: do catch处理异常, 能够接受并且输出错误
+        do{
+            let json3 = try JSONSerialization.jsonObject(with: data!, options: [])
+            print(json3)
+        }catch{
+            print(error)
+        }
+
+
     }
 
     override func didReceiveMemoryWarning() {
