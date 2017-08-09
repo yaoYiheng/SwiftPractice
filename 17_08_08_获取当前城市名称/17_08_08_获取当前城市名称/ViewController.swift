1aa713b45b0cd4e63279cf2fc7a45adbd544e8f2
YihengYao <fn1117@qq.com>
2017-08-09 22:59:13 +1200

    17_08_08_获取当前城市名称

---
+++ 17_08_08_获取当前城市名称/17_08_08_获取当前城市名称/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  17_08_08_获取当前城市名称
+//
+//  Created by 姚懿恒 on 2017/8/9.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController {
+    @IBOutlet weak var placeLabel: UILabel!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        <#code#>
+    }
+
+}
+
