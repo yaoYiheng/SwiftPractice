fe84b2b504e0a35268cab5b44bbea919722265b8
YihengYao <fn1117@qq.com>
2017-08-10 23:28:50 +1200

    17_08_10_使用第三方定位
    注意点:
    如果第三方框架是OC, 需要用在Swift项目中时, 需要创建桥接文件, 一般是.h文件, 然后在BuildSetting中搜索
    Bridge, 输入两级的.h文件路径即可

---
+++ 17_08_08_获取当前城市名称/17_08_08_获取当前城市名称/Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Bridge.h
+//  17_08_08_获取当前城市名称
+//
+//  Created by 姚懿恒 on 2017/8/10.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+
+#endif /* Bridge_h */
