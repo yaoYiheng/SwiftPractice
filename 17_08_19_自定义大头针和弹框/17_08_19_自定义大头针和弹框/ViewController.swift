a2fab759dcfbafdd7e589c54ba9f29ade50b6445
YihengYao <fn1117@qq.com>
2017-08-20 22:27:38 +1200

    自定义大头针视图
    /如果想要自定义大头针视图, 需要继承MKAnnotationView, 十分重要!
    leftCalloutAccessoryView
    rightCalloutAccessoryView
    detailCalloutAccessoryView
    为自定义弹框添加左右视图以及详细视图

--- 17_08_19_自定义大头针和弹框/17_08_19_自定义大头针和弹框/ViewController.swift
+++ 17_08_19_自定义大头针和弹框/17_08_19_自定义大头针和弹框/ViewController.swift
@@ -66,18 +66,45 @@ extension ViewController: MKMapViewDelegate{
     /// 返回大头针视图
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
 
-        let identifier = "item"
+        let iden = "item"
 
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
-
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: iden)
         if annotationView == nil {
-            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            //如果想要自定义大头针视图, 需要继承MKAnnotationView, 十分重要!
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: iden)
+        }
+        //重新为模型对象赋值, 非常重要
+        annotationView?.annotation = annotation
+
+        //自定义大头针的图片
+        annotationView?.image = UIImage(named: "category_5")
+
+
+        //创建弹框左右侧图片
+        let imageView = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: 40, height: 40))
+        let image = UIImage.init(named: "htl.jpg")
+        imageView.image = image
+
+        annotationView?.leftCalloutAccessoryView = imageView
+
+        let imageView1 = UIImageView.init(frame: CGRect.init(x: 0, y: 0, width: 40, height: 40))
+        let image1 = UIImage.init(named: "eason.jpg")
+        imageView1.image = image1
+
+        annotationView?.rightCalloutAccessoryView = imageView1
+
+        //添加详细视图
+        if #available(iOS 9.0, *) {
+            annotationView?.detailCalloutAccessoryView = UISwitch()
         }
 
+
         //修改大头针颜色
-        annotationView?.pinTintColor = UIColor.purple
+        if #available(iOS 9.0, *) {
+//            annotationView?.pinTintColor = UIColor.purple
+        }
         //允许动画下落
-        annotationView?.animatesDrop = true
+//        annotationView?.animatesDrop = true
         //弹框
         annotationView?.canShowCallout = true
         
