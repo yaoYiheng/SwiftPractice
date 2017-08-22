48239ad8353b9c1c36d6606da9c2b16391998463
YihengYao <fn1117@qq.com>
2017-08-22 22:29:32 +1200

    17_08_22_设置相关代理

--- 17_08_22_测试划线/17_08_22_测试划线/ViewController.swift
+++ 17_08_22_测试划线/17_08_22_测试划线/ViewController.swift
@@ -7,17 +7,65 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    var mapView: MKMapView!
+
+    private lazy var locationManager: CLLocationManager = {
+        let locationManager = CLLocationManager()
+
+        locationManager.requestAlwaysAuthorization()
+
+        //设置位置管理者的代理
+        locationManager.delegate = self
+
+
+        return locationManager
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        _ = locationManager
+        initMapView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func initMapView() {
+        mapView = MKMapView()
+        mapView.delegate = self
+        mapView.frame = view.bounds
+        mapView.mapType = .hybrid
+
+        mapView.userTrackingMode = .followWithHeading
+//        mapView.showsUserLocation = true
+        view.addSubview(mapView)
+    }
+
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        locationManager.startUpdatingLocation()
+    }
+}
+
+
+// MARK: - <#CLLocationManagerDelegate#>
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {return}
+        if location.horizontalAccuracy < 0 {return}
+
+        print(location)
+    }
+
+
+}
+// MARK: - <#MKMapViewDelegate#>
+extension ViewController: MKMapViewDelegate{
+
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        <#code#>
     }
 
 
