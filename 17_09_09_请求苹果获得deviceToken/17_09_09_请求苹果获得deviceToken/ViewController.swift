2a95624baf6fc8d802e1192752717b8a46f45d1e
YihengYao <fn1117@qq.com>
2017-09-09 23:19:07 +1200

    请求苹果获得deviceToken

---
+++ 17_09_09_请求苹果获得deviceToken/17_09_09_请求苹果获得deviceToken/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17_09_09_请求苹果获得deviceToken
+//
+//  Created by 姚懿恒 on 2017/9/9.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
