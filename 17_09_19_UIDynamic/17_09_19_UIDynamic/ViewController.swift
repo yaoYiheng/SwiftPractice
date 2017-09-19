d37239bb780205f5f9bbe4c6bbc7b812073b2769
YihengYao <fn1117@qq.com>
2017-09-19 23:25:26 +1200

    17_09_19_推动模仿UIPushBehavior

--- 17_09_19_UIDynamic/17_09_19_UIDynamic/ViewController.swift
+++ 17_09_19_UIDynamic/17_09_19_UIDynamic/ViewController.swift
@@ -30,10 +30,16 @@ class ViewController: UIViewController {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        push()
         //执行重力仿真行为
         gravity()
+
+        //执行碰撞行为
+        collision()
+
     }
 
+    /// 重力仿真
     private func gravity() {
 
         //创建仿真行为
@@ -48,6 +54,34 @@ class ViewController: UIViewController {
 
     }
 
+    /// 碰撞仿真
+    private func collision() {
+
+
+    }
+
+    /// 捕捉仿真
+    private func snap() {
+
+    }
+
+    /// 推动仿真
+    private func push() {
+        //
+        //创建推动仿真行为
+        //        continuous: 推动力会一直作用在物体上
+        //        instantaneous: 不会持续, 推过之后物理保持当前状态
+        let push = UIPushBehavior(items: [box1], mode: .continuous)
+
+        //还需要设置推的方向数值越大速度越快
+        push.pushDirection = CGVector.init(dx: 0, dy: -1)
+
+        animator.removeBehavior(push)
+
+        animator.addBehavior(push)
+
+
+    }
 
 }
 
