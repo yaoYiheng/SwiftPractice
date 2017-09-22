6e453fa0a3547a5d5b6a38222cee4229ad9d5b74
YihengYao <fn1117@qq.com>
2017-09-22 23:51:05 +1200

    LSApplicationQueriesSchemes白名单数组与UIApplicationOpenURLOptionUniversalLinksOnly
            //9.0之后需要在info.plist文件中设置
            // LSApplicationQueriesSchemes 数组: 添加白名单
    
            //关于这个key: UIApplicationOpenURLOptionUniversalLinksOnly
            //如果将其设置为true, 只会当手机中装有该app时, 才会跳转,
            //如果设置为false, 手机中没有该app会从网页访问

--- 17_09_22_不同应用间的跳转/17_09_22_不同应用间的跳转/ViewController.swift
+++ 17_09_22_不同应用间的跳转/17_09_22_不同应用间的跳转/ViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        //iOS 9.0之前可以用以下方法打开
+        //9.0之后需要在info.plist文件中设置
+        // LSApplicationQueriesSchemes 数组: 添加白名单
+//        let url = NSURL.init(string: "sms://10086")
+//        UIApplication.shared.openURL(url! as URL)
 
+        let url = URL.init(string: "https://twitter.com/kharrison")
+
+        //关于这个key: UIApplicationOpenURLOptionUniversalLinksOnly
+        //如果将其设置为true, 只会当手机中装有该app时, 才会跳转,
+        //如果设置为false, 手机中没有该app会从网页访问
+        let options = [UIApplicationOpenURLOptionUniversalLinksOnly : false]
+//        UIApplication.shared.open(url!, options: options, completionHandler: nil)
+        UIApplication.shared.open(url!, options: options, completionHandler: nil)
+
+    }
 
 }
 
