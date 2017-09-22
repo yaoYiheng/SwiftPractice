7db9f99fb66cbb1b4fba7efce0de2a89659f0173
YihengYao <fn1117@qq.com>
2017-09-22 23:14:33 +1200

    17_09_22_不同应用间的跳转

---
+++ 17_09_22_不同应用间的跳转/17_09_22_不同应用间的跳转/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17_09_22_不同应用间的跳转
+//
+//  Created by 姚懿恒 on 2017/9/22.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
