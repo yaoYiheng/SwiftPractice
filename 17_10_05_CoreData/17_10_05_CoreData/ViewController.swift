3897987bc95240cdf46245e3fadabaea0a7d0bf3
YihengYao <fn1117@qq.com>
2017-10-06 23:07:29 +1300

    17_10_06_需要重开一个项目

--- 17_10_05_CoreData/17_10_05_CoreData/ViewController.swift
+++ 17_10_05_CoreData/17_10_05_CoreData/ViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //生成上下文, 关联文件模型生成数据库
+
+        //1. 获取上下文
+        let context = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+        //上下文关联数据库
+        let model = NSManagedObjectModel.mergedModel(from: nil)
+
+        //持久化存储调度器
+        let store = NSPersistentStoreCoordinator.init(managedObjectModel: model!)
+
+        //告诉store数据库的路径
+
+        let path = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, .allDomainsMask, true).first
+        let urlPath = URL.init(string: path!)
+
+        let url = urlPath?.appendingPathComponent("company.sqlite")
+
+        
+        store.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url!, options: <#T##[AnyHashable : Any]?#>)
+
+
+
+        context.persistentStoreCoordinator = store
+
     }
 
 
