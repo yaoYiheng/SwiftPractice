3c8dfb5ffc94358a9dad439ac6ecf866786d3f6f
YihengYao <fn1117@qq.com>
2017-10-06 23:37:04 +1300

    17_10_06_添加

--- 17_10_06_CoreData2/17_10_06_CoreData2/ViewController.swift
+++ 17_10_06_CoreData2/17_10_06_CoreData2/ViewController.swift
@@ -7,17 +7,35 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //获取context
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+        //添加50个员工
+        for item in 0...50 {
+            let employee = NSEntityDescription.insertNewObject(forEntityName: "Employee", into: context) as! Employee
+            employee.name = "张\(item)"
+            employee.height = Float(item)
+            employee.birthday = NSDate()
+
+        }
+        if context.hasChanges {
+            do {
+                print("保存成功")
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+
+
     }
 
 
