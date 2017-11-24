405c6d0f798819156cc3ef70ba27128a28bee2fc
YihengYao <fn1117@qq.com>
2017-11-24 22:54:33 +1300

    17_11_24_将选中的菜单显示到对应的Label上

--- 17_11_23_Order/17_11_23_Order/ViewController.swift
+++ 17_11_23_Order/17_11_23_Order/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var fruitLabel: UILabel!
+    @IBOutlet weak var dishesLabel: UILabel!
+    @IBOutlet weak var liquidLabel: UILabel!
+    
      lazy var food: [Array<Any>] = {
         //从Bundle中加载plist文件
         let path = Bundle.main.path(forResource: "Food.plist", ofType: nil)
@@ -39,6 +42,21 @@ extension ViewController: UIPickerViewDelegate{
 
     }
 
+    //代理方法, 当选中到某一组的某一行时会调用该方法
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selFood = food[component][row] as? String
+
+        switch component {
+        case 0:
+            fruitLabel.text = selFood
+        case 1:
+            dishesLabel.text = selFood
+        case 2:
+            liquidLabel.text = selFood
+        default:
+            break
+        }
+    }
 
 }
 extension ViewController: UIPickerViewDataSource{
