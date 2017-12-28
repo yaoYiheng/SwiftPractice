7d294dde1f9d942bfb588874eb5066c4c46165e1
YihengYao <fn1117@qq.com>
2017-12-28 21:51:35 +1300

    17_12_28_导入第三方
    添加桥接文件
    在Swift项目中使用OC文件, 随便创建一个类, 在提示框中确认创建桥接文件, 后删除创建的类, 在桥接文件中导入.h文件即可

---
+++ 17_12_22_通讯录/Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  17_12_22_通讯录
+//
+//  Created by 姚懿恒 on 2017/12/28.
+//  Copyright © 2017年 姚懿恒. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
